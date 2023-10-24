08ec2b26f325f994499e77a5a9204ab3f7964799
Bernard Allan <chwakik@gmail.com>
2023-10-24 09:00:49 +0300

    main

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\LogoutController;
 use App\Http\Controllers\Api\Auth\RegisterController;
-use App\Http\Controllers\Api\V1\TaskController;
-use App\Http\Controllers\Api\V1\CompleteTaskController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +18,7 @@ use App\Http\Controllers\Api\V1\CompleteTaskController;
 |
 */
 
-// require __DIR__ . '/api/v1.php';
+require __DIR__ . '/api/v1.php';
 require __DIR__ . '/api/v2.php';
 
 Route::prefix('auth')->group(function () {
