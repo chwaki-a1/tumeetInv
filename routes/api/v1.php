7323a2fef18a036884dc0cbe4d93e474bfa233d8
Bernard Allan <chwakik@gmail.com>
2023-11-07 15:32:37 +0300

    auth

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TaskController;
 use App\Http\Controllers\Api\V1\CompleteTaskController;
 
+
+//no authentication
+
 Route::prefix('v1')->group(function () {
+
     Route::apiResource('/tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
--- routes/api/v2.php
+++ routes/api/v2.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V2\TaskController;
 use App\Http\Controllers\Api\V2\CompleteTaskController;
 
+//with auth using sanctum
+
 Route::middleware('auth:sanctum')->prefix('v2')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
